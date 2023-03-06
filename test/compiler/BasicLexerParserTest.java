ae923f58f4b2b3de6caddd2a44e9659758ed12dc
Alessandro <alemazzoli97@gmail.com>
2023-03-06 21:08:06 +0100

    Initial commit

---
+++ test/compiler/BasicLexerParserTest.java
@@ -0,0 +1,50 @@
+package compiler;
+
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+
+public class BasicLexerParserTest {
+
+    private FOOLLexer lexer;
+    private FOOLParser parser;
+
+
+    @BeforeEach
+    void setUp() throws Exception {
+        String fileName = "examples/prova.fool";
+        final CharStream chars = CharStreams.fromFileName(fileName);
+        lexer = new FOOLLexer(chars);
+        final CommonTokenStream tokens = new CommonTokenStream(lexer);
+        parser = new FOOLParser(tokens);
+    }
+
+    @Test
+    void testWrongFileName() {
+        final String wrongFileName = "examples/wrong.fool";
+        assertThrows(IOException.class, () -> CharStreams.fromFileName(wrongFileName));
+    }
+
+
+    @Test
+    void testZeroLexicalErrors() {
+        parser.prog();
+        assertEquals(0, lexer.lexicalErrors);
+    }
+
+    @Test
+    void testZeroSyntaxErrors() {
+        parser.prog();
+        assertEquals(0, parser.getNumberOfSyntaxErrors());
+    }
+
+
+}
---
+++ test/compiler/utils/CompilerUtils.java
@@ -0,0 +1,6 @@
+package compiler.utils;
+
+public class CompilerUtils {
+
+    
+}
