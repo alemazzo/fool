31abc8912c1ea9a038a98c695740df80eb91f1ae
Alessandro Mazzoli <alessandro.mazzoli9@studio.unibo.it>
2023-05-06 12:04:06 +0200

    Removed comment from TypeRels

--- src/compiler/TypeRels.java
+++ src/compiler/TypeRels.java
@@ -3,13 +3,13 @@ package compiler;
 import compiler.AST.BoolTypeNode;
 import compiler.AST.IntTypeNode;
 import compiler.lib.TypeNode;
+
 import java.util.HashMap;
 import java.util.Map;
 
 public class TypeRels {
     public static Map<String, String> superType = new HashMap<>();
 
-    // valuta se il tipo "a" e' <= al tipo "b", dove "a" e "b" sono tipi di base: IntTypeNode o BoolTypeNode
     public static boolean isSubtype(TypeNode a, TypeNode b) {
         return isEqual(a, b)
                 || isBoolAndInt(a, b);
